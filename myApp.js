0bd12de5d4f34fdd1df36469a8f65a13c2b5c596
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-20 12:52:01 -0600

    add schema

--- myApp.js
+++ myApp.js
@@ -1,8 +1,19 @@
-require('dotenv').config();
-var mongoose = require('mongoose');
+require("dotenv").config();
+const { Schema } = require("mongoose");
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-let Person;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var Person = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
