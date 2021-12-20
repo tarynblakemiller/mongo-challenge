0c41571730015aa25d1ce67b524327d14526dafa
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-20 12:53:01 -0600

    add person model

--- myApp.js
+++ myApp.js
@@ -3,17 +3,18 @@ const { Schema } = require("mongoose");
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-var Person = new Schema({
+var personSchema = new Schema({
   name: { type: String, required: true },
   age: Number,
   favoriteFoods: [String],
 });
+
+var Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
