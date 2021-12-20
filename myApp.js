eef142fc1e5ac680b0576aaee479f26c11caa64f
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-20 12:41:22 -0600

    conncting to mongodb

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
+require('mongoose');
 
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let Person;
 
 const createAndSavePerson = (done) => {
