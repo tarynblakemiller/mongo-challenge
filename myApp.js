43a2d990244877bb33ba67e60bcf38b329a6a887
Taryn Miller <yourfriendtaryn@gmail.com>
2021-12-20 12:43:38 -0600

    defined mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-require('mongoose');
+var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let Person;
